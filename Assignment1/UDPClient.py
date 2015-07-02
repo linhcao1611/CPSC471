423d050ef8d8a337ea43dde4890d7f04a71ae863
linhcao1611 <linhcao@csu.fullerton.edu>
2015-07-02 16:50:50 -0700

    add comments

--- Assignment1/UDPClient.py
+++ Assignment1/UDPClient.py
@@ -5,21 +5,27 @@ serverName = 'localhost'
 #serverName = 'google.com'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-
 ping = bytes("PING", "utf-8")
 
-
+# this function will send a message to server and return
+# ping time if the server responses, return None if server
+# does not reponse 
 def ping_server():
+    # using settimeout socket's built-in function 
     clientSocket.settimeout(1)
+    # while not timeout
     try:
         start = time.perf_counter()
         clientSocket.sendto(ping,(serverName, serverPort))
         response, serverAddress = clientSocket.recvfrom(2048)
         end = time.perf_counter()
         return end-start
+    # when timeout
     except timeout:
         return None
 
+# main function will ping to server 10 times and calculate
+# the avg ping time
 sum = 0
 success = 0
 for i in range(1,11):
@@ -34,7 +40,7 @@ if(success !=0):
     print('Avg ping time = ' + str(sum/success) + ' seconds')
     print('There are ' + str(success) + ' success packet')
     
-
+raw_input()
 clientSocket.close()
 
 
--- Assignment1/UDPClient_v2.py
+++ Assignment1/UDPClient_v2.py
@@ -6,21 +6,26 @@ serverName = 'localhost'
 #serverName = 'google.com'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-
 ping = bytes("PING", "utf-8")
 
-
+# this function will send a message to server and return
+# ping time if the server responses, return None if server
+# does not reponse 
 def ping_server():
-    clientSocket.settimeout(1)
+    # using settimeout socket's built-in function 
+    clientSocket.settimeout(0.00001) # set timeout 1 second
+    # while not timeout
     try:
         start = time.perf_counter()
         clientSocket.sendto(ping,(serverName, serverPort))
         response, serverAddress = clientSocket.recvfrom(2048)
         end = time.perf_counter()
         return end-start
+    # if timeout
     except timeout:
         return None
 
+# main function will ping to server until user shutdowns the program
 sum = 0
 success = 0
 i = 0
@@ -38,8 +43,6 @@ while 1:
         print('Avg ping time = ' + str(round(sum/success,5)) + ' seconds')
         print('Success ping rate = ' + str(round((success*100/i),5)) + '%')
         
-
-
 clientSocket.close()
 
 
